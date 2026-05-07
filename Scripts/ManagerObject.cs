b4073c344768b44fb90cd85770f7849a0ddb37ee
Trong Nguyen <nguyentrong101094@gmail.com>
2026-05-07 16:11:58 +0700

    fix input system symbol and dotween asmdef

--- Scripts/ManagerObject.cs
+++ Scripts/ManagerObject.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+#if ENABLE_INPUT_SYSTEM
 using UnityEngine.InputSystem;
+#endif
 using UnityEngine.UI;
 
 namespace Omnilatent.ScenesManager
